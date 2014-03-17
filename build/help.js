d346da315b25ae48895568d84c59f45553f0cbd2
Joshwa Fugett <jfugett27@gmail.com>
2014-03-17 18:42:07 -0400

    chore(build): Cleaned up comments in initial setup so it's easier to follow

--- build/help.js
+++ build/help.js
@@ -1,21 +1,30 @@
 'use strict';
 
+// this is the module that handles listing out the tasks in a better format
 var lister = require('gulp-task-listing');
 
+// this is our base object
 var help = {};
 
+// this function just makes gulp and the tasks object available to the other items
 help.init = function init(gulp, tasks){
+    // keep a reference to gulp
     help.gulp = gulp;
     
+    // keep a reference to the tasks object
     help.tasks = tasks;
     
+    // setup the base tasks
     help.setupTasks();
     
+    // return the tasks incase they need to be used again
     return help.tasks;
 };
 
+// this method adds the help task to the tasks array
 help.setupTasks = function setupTasks(){
     help.tasks.help = lister;
 };
 
+// export our object
 module.exports = help;
--- build/index.js
+++ build/index.js
@@ -1,44 +1,63 @@
 'use strict';
 
+// this is just our base object
 var builder = {};
 
+// this method is the primary runner
 builder.init = function init(gulp){
+    // keep a reference to gulp
     builder.gulp = gulp;
+    
+    // create an object to store tasks in
     builder.tasks = {};
     
+    // setup our base tasks
     builder.setupTasks();
     
+    // setup any tasks that need to be included from our modules
     builder.includeTasks();
     
+    // return the tasks so that they can be used externally
     return builder.tasks;
 };
 
+// this method sets up our main tasks that we'll be running
 builder.setupTasks = function setupTasks(){
+    // just a shorthand reference
     var tasks = builder.tasks;
     
+    // assign the default task to dev
     tasks.default = builder.dev;
     
+    // create the development task
     tasks.dev = builder.dev;
     
+    // create the test task
     tasks.test = builder.test;
     
+    // create the build task
     tasks.build = builder.build;
 };
 
+// this method is responsible for calling in tasks defined in other files
 builder.includeTasks = function includeTasks(){
     require('./help').init(builder.gulp, builder.tasks);
 };
 
+// this is the development method that defines our default development workflow
 builder.dev = function dev(){
     console.log('dev method called');
 };
 
+// this is the test method that runs all of our tests
 builder.test = function test(){
     console.log('test method called');
 };
 
+// this is our build method that creates the actual build from our source files
 builder.build = function build(){
     console.log('build method called');
 };
 
+// export our object
 module.exports = builder;
